94ceeedd7126902ccf807fd20f5240f626654080
Prakhar Agarwal <prakhargvp@gmail.com>
2020-03-08 22:47:11 +0530

    Blog Archive Page

--- wp-content/themes/fictional-university-theme/archive.php
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -5,10 +5,10 @@
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title">
-        <?php single_post_title() ?>    
+        <?php the_archive_title()?>   
       </h1>
       <div class="page-banner__intro">
-        <p>Keep up our latest news!</p>
+        <p><?php the_archive_description()?></p>
       </div>
     </div>  
   </div>
