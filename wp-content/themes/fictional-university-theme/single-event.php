5dfbd1a8f7180467e1850628ff576bd164ad5e1d
Prakhar Agarwal <prakhargvp@gmail.com>
2020-04-08 07:51:39 +0530

    Program Post Type: Displaying Relationship(Front-End)

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -15,9 +15,21 @@
   <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?=get_post_type_archive_link('event')?>"><i class="fa fa-home" aria-hidden="true"></i> Event Home</a> <span class="metabox__main"><?=the_title()?></span></p>
     </div>
-  <div class="generic-content">
-    <p><?php the_content() ?></p>
-  </div>
+  <div class="generic-content"><?php the_content() ?></div>
+
+  <?php
+    $relatedPrograms = get_field('related_programs');
+    if($relatedPrograms) {
+    echo '<hr class="section-break">';
+    echo '<h2 class="headline headline--medium">Related Programs(s)</h2>';
+    echo '<ul class="link-list min-list">';
+    foreach ($relatedPrograms as $program) { ?>
+    <li><a href="<?=get_the_permalink($program)?>">
+      <?=get_the_title($program)?>
+    </a></li>
+    <?php  } echo '</ul>';  } ?>
+   
+
  </div>
 <?php } 
   get_footer();
--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -14,10 +14,53 @@
  <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?=get_post_type_archive_link('program')?>"><i class="fa fa-home" aria-hidden="true"></i> Program Home</a> <span class="metabox__main"><?=the_title()?></span></p>
-    </div>
-  <div class="generic-content">
-    <p><?php the_content() ?></p>
   </div>
+  <div class="generic-content"><?php the_content() ?></div>
+  <?php
+    $today = date('Ymd');
+    $homePageEvents = new WP_Query([
+      'posts_per_page' => -1,
+      'post_type' => 'event',
+      'meta_key' => 'event_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC',
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        ),
+        array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"'. get_the_ID() . '"'
+        )
+      )
+    ]);
+
+    while($homePageEvents->have_posts()){
+      $homePageEvents->the_post();
+    ?>
+    <div class="event-summary">
+      <a class="event-summary__date t-center" href="#">
+        <span class="event-summary__month">
+          <?php
+          $eventDate = new DateTime(get_field('event_date'));
+          echo $eventDate->format('M');
+          ?>
+        </span>
+        <span class="event-summary__day"><?php echo $eventDate->format('d')?></span>  
+      </a>
+      <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+        <p>
+          <?php echo wp_trim_words(get_the_content(), 18); ?>
+          <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+        </p>
+      </div>
+    </div>
+  <?php } ?>
  </div>
 <?php } 
   get_footer();
