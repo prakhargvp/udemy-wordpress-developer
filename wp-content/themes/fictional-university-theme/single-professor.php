ff017ad3c1a59a6a354ee74c31d8d45e9335603a
Prakhar Agarwal <prakhargvp@gmail.com>
2020-05-08 07:56:20 +0530

    Professor Post Type: Featured Image Sizes & Cropping

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -15,7 +15,7 @@
  
   <div class="generic-content">
     <div class="row group">
-      <div class="one-third"><?php the_post_thumbnail(); ?></div>
+      <div class="one-third"><?php the_post_thumbnail('professorPortrait'); ?></div>
       <div class="two-third"><?php the_content(); ?></div>
     </div>
    </div>
